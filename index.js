c23640f97e34137654af2b636492ba62e51f6852
ge9 <kensho.tsurusaki@gmail.com>
2021-10-28 09:05:43 +0900

    first commit (set up rollup to use in browser)

---
+++ index.js
@@ -0,0 +1,12 @@
+export { Kage, FONTTYPE } from "./lib/kage";
+export { Polygons } from "./lib/polygons";
+//export const kage_export = {Kage: Kage, FONTTYPE: FONTTYPE, Polygons: Polygons};
+// Object.defineProperty(exports, "__esModule", { value: true });
+// var kage_1 = require("./lib/kage");
+// Object.defineProperty(exports, "Kage", { enumerable: true, get: function () { return kage_1.Kage; } });
+// var polygons_1 = require("./lib/polygons");
+// Object.defineProperty(exports, "Polygons", { enumerable: true, get: function () { return polygons_1.Polygons; } });
+// var buhin_1 = require("./lib/buhin");
+// Object.defineProperty(exports, "Buhin", { enumerable: true, get: function () { return buhin_1.Buhin; } });
+// var kage_2 = require("./lib/kage");
+// exports.default = kage_2.Kage;
---
+++ rollup.config.js
@@ -0,0 +1,8 @@
+export default {
+  input: 'index.js',
+  output: {
+    file: 'dist/kage_bundle.js',
+    format: 'iife',
+    name: 'kage_export'
+  }
+};
